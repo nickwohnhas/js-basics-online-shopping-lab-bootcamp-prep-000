5e4c11875b6b51aad82a5739d3f04a0e8aec553b
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-08 23:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
     return "Your shopping cart is empty."
   }
   else if (cart.length == 1){
-    return `In your cart, you have ${cartname1} at `
+    return `In your cart, you have ${cartname1} at $${cartprice1}.`
   }
     
 }
