dc0a018755b3899e1334943933d1b180da910184
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 02:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   cart.length = 0
-    return `Your total cost is $${new_total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${new_total}, which will be charged to the card ${cardNumber}.`
   
 }
