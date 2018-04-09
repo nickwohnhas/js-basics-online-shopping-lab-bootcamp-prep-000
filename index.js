40ee5835964c95989f9e6b162365945fdcca0ee9
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 01:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,11 +71,12 @@ function removeFromCart(item) {
     var objectname = object.itemName
     if (item == objectname){
       cart.splice(i,1)
-      return cart
+      
       }
       else {
         return `That item is not in your cart.`
       }
+ return cart 
   }
 }
 
