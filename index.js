a6fa1b288f60650269c4beb60bf0346d6339a2e5
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 01:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,11 +73,11 @@ function removeFromCart(item) {
     cart.splice(i,1)
       
       }
-      else {
-        return `That item is not in your cart.`
-      }
+      
+        
+    
     }
-    return cart
+    return `That item is not in your cart.`
 }
 
 function placeOrder(cardNumber) {
