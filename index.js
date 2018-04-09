986ecb5a5fa278c755ad3215af480ed4fea5d4a8
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 01:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,8 +76,10 @@ function removeFromCart(item) {
       else {
         return `That item is not in your cart.`
       }
- return cart 
+ 
   }
+
+  return cart
 }
 
 function placeOrder(cardNumber) {
