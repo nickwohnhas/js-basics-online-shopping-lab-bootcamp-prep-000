f87147918aeb95702155fa4609fd3734446be1b1
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 02:31:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,10 +81,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var new_total = total()
   if (cardNumber === undefined){
     return `Sorry, we don't have a credit card on file for you.`
   }
-  
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart.length = 0
+    return `Your total cost is $${new_total()}, which will be charged to the card ${cardNumber}.`
   
 }
