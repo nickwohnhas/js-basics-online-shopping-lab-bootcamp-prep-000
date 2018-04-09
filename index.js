32120930883f103745deace49a8ae6ccd7a9827e
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-09 02:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,9 +86,10 @@ function placeOrder(cardNumber) {
   }
   else {
     
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+   cart.length = 0
   
     
     }
-
+ return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  
 }
